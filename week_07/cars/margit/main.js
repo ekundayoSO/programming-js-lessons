370db432d016fbaf10d1472b3eab81602b20d111
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-03-01 12:47:26 +0200

    add cars html

---
+++ week_07/cars/margit/main.js
@@ -0,0 +1,54 @@
+'use strict';
+const addCarForm = document.querySelector("#addCar");
+
+const cars  = [];
+
+class Car {
+    constructor(license, maker, model, owner, price, color) {
+        this.license = license;
+        this.maker = maker;
+        this.model = model;
+        this.owner = owner;
+        this.price = price;
+        this.color = color;
+    }
+}
+
+const addCar = (event) => {
+    // This is important for form "submit button" and not "click button" and to prevent default behaviour in console.
+    event.preventDefault();
+
+    const license = document.querySelector("#license").value;
+    const maker = document.querySelector("#maker").value;
+    const model = document.querySelector("#model").value;
+    const owner = document.querySelector("#owner").value;
+    const price = document.querySelector("#price").value;
+    const color = document.querySelector("#color").value;
+
+    const newCar = new Car(license, maker, model, owner, price, color);
+
+    cars.push(newCar);
+
+    displayTable();
+
+    addCarForm.reset();
+
+}
+
+const displayTable = () => {
+    const table = document.querySelector("#carsTable");
+    table.innerHTML = table.rows[0].innerHTML;
+
+    cars.forEach((car) => {
+        const row = table.insertRow(-1);
+
+        Object.values(car).forEach(text => {
+            const cell = row.insertCell(-1);
+            cell.textContent = text;
+        });
+    });
+};
+
+addCarForm.addEventListener("submit", addCar);
+
+
